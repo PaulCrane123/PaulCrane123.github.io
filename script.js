8005357b25a7f2e9ef3569faa4ff6fd5590288e8
PaulCrane123 <121836683+PaulCrane123@users.noreply.github.com>
2023-03-03 17:32:11 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var function1 = function1;
+var newpg = "";
+var newpg_pt2 = "";
+function function1() {
+  alert("Thank you for signing in!");
+  let userinput = document.getElementById("userInput").value;
+  let passinput = document.getElementById("passInput").value;
+  alert("Your username is " + userinput +"\rYour password is " + passinput);
+}
---
+++ sign-in-test-for-css.markdown
@@ -0,0 +1,7 @@
+Sign in Test For CSS
+--------------------
+
+
+A [Pen](https://codepen.io/Paul-Crane/pen/rNZNdmV) by [Paul Crane](https://codepen.io/Paul-Crane) on [CodePen](https://codepen.io).
+
+[License](https://codepen.io/license/pen/rNZNdmV).
